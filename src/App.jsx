61c11d58d926d7e8094e0a018df0ed5357084a0d
Gekko29 <askatec@gmail.com>
2026-01-13 10:51:10 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2879,6 +2879,7 @@ try {
       </AccordionSection>
     </div>
       </div>
+)}
 
       <div className="bg-gray-900 text-white py-8 mt-12">
         <div className="max-w-6xl mx-auto px-4 text-center">
