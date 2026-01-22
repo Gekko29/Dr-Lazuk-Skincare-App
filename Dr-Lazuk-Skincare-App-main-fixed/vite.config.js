c286b5c3dec7737b4ea80218d4fb5f044a5dbbb1
Gekko29 <askatec@gmail.com>
2026-01-22 16:56:04 +0000

    Apply Option A spec-compliant overwrite package

--- Dr-Lazuk-Skincare-App-main-fixed/vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js - ESM version
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js (CommonJS)
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
